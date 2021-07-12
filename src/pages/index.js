5707acf91e8d4da6d07f97f7f216f286946326a1
Olvine George <olivinegeorge@gmail.com>
2021-07-12 22:42:10 +0300

    second commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,18 @@
 import * as React from 'react'
 import {Link} from 'gatsby';
 import Layout from '../components/layout';
+import { StaticImage } from 'gatsby-plugin-image';
 
 
 const IndexPage = () => {
   return (
      <Layout pageTitle = "Home Page">
        <p>I am trying to make a blog and change the world</p>
+
+       <StaticImage src = "../images/love.jpg" />
+
+     
+
      </Layout>
   )
 }
